7ae48b4cb775f557e84afdafa0d0adbd7f2bd59f
Jouni Johansson <bgu211@myy.haaga-helia.fi>
2023-02-07 18:58:48 +0200

    clean up

--- src/components/appBar.js
+++ src/components/appBar.js
@@ -37,7 +37,7 @@ function ResponsiveAppBar() {
             variant="h6"
             noWrap
             component="a"
-            href="/"
+            href="/homepage"
             sx={{
               mr: 2,
               display: { xs: 'none', md: 'flex' },
